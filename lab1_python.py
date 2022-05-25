6010e36a074a5b43b181f12120264db154b9fef2
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2022-05-26 01:01:21 +0530

    Create lab1_python.py

---
+++ lab1_python.py
@@ -0,0 +1,11 @@
+sp=int(input("enter selling price"))
+print("your cost price is=%f"%(sp-0.2*sp))
+a=20
+b=30
+print(f'he{a}ll{b}o')                               
+name =input("your name")
+roll =input("roll number")
+university =input("university name")
+print(f'''name ={name} 
+roll number ={roll}
+university name = {university}''')
