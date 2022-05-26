e324cde1ac1ac7d31c786cf51b29efdadfec1301
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2022-05-26 10:40:33 +0530

    Create set_default.py

---
+++ set_default.py
@@ -0,0 +1,7 @@
+car={"brand":"ford","model":"mustang","year":1964}
+x=car.setdefault("colour","white")
+y=car.setdefault("model","bromo")
+z=car.setdefault("brand","raj")
+print(x)
+print(y)
+print(z)
