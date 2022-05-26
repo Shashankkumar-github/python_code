0df63c7fbab790ac14de648d3c489a405cf24a49
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2022-05-26 10:44:08 +0530

    Create vowel_or_not.py

---
+++ vowel_or_not.py
@@ -0,0 +1,5 @@
+x=input("enter the character")
+if(x=='e' or x=='a' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U'):
+    print("vowel")
+else:
+    print("not vowel")
